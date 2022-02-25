945c0f830db987bffabfb0f97a3b6120db6511ff
Owen Hill <47730337+thisisowenhill@users.noreply.github.com>
2022-02-25 17:33:30 -0600

    Comments and resolution
    - Added more detail for the comments 
    - Made the resolution of the image based off of the user's primary display resolution
    - Added a new directory to store the images

--- generate_background.py
+++ generate_background.py
@@ -1,12 +1,7 @@
-"""
-TODO
-Make the images appear in a new folder inside the cwd
-Add comments
-"""
 from datetime import date
 from pathlib import Path
 import schedule as schedule
-from PIL import Image, ImageDraw, ImageFont
+from PIL import Image, ImageDraw, ImageFont, ImageGrab
 import os
 import ctypes
 import time
@@ -14,14 +9,25 @@ import logging
 
 
 def create_image(day: date):
-    filename = day.isoformat() + ".png"
-    im = Image.new("RGB", (2560, 1440), (19, 19, 19))
+    """
+     Calculate the size of the user's primary display using the dimensions of a screenshot and then
+     create the new background image using Pillow and store it into the new directory.
+    """
+    filename = "backgroundimages/" + day.isoformat() + ".png"
+    ss = ImageGrab.grab()
+    im = Image.new("RGB", ss.size, (19, 19, 19))
     datefont = ImageFont.truetype("corbelb.ttf", 60)
     dayfont = ImageFont.truetype("corbel.ttf", 50)
     d = ImageDraw.Draw(im)
 
-    d.text((1280, 1440/2-30), date.strftime(day, "%B - %d - %Y"), font=datefont, fill=(255, 255, 255, 255), anchor="mm")
-    d.text((1280, 1440/2+30), date.strftime(day, "%A"), font=dayfont, fill=(200, 200, 200, 255), anchor="mm")
+    d.text((ss.size[0]/2, ss.size[1]/2-30),
+           date.strftime(day, "%B - %d - %Y"),
+           font=datefont, fill=(255, 255, 255, 255),
+           anchor="mm")
+    d.text((ss.size[0]/2, ss.size[1]/2+30),
+           date.strftime(day, "%A"),
+           font=dayfont, fill=(200, 200, 200, 255),
+           anchor="mm")
 
     im.save(filename)
 
@@ -29,6 +35,10 @@ def create_image(day: date):
 
 
 def check_date():
+    """
+     Grab/Store the current date and then subtract one day and store that as "yesterday".
+     If the "yesterday" isn't there then it's safe to assume that it's a new day of a new month/year.
+    """
     today = date.today()
     yesterday = date(today.year, today.month, today.day)
     try:
@@ -36,18 +46,26 @@ def check_date():
     except ValueError:
         logging.error("ValueError: could not subtract 1 from \"yesterday\" day variable")
         pass
-
-    if not Path(today.isoformat() + ".png").exists():
+    # Check to see if the current day's image isn't there and then create an image if it's not.
+    if not Path("backgroundimages/" + today.isoformat() + ".png").exists():
         if yesterday <= today:
             create_image(today)
 
 
 def set_wallpaper(directory: str):
+    """
+     This is magic.
+    """
     ctypes.windll.user32.SystemParametersInfoW(20, 0, directory, 0)
 
 
 def delete_previous():
-    allfiles = sorted(Path(".").glob("[0-9]*.py"))
+    """
+     Once every sunday this function will:
+      - Sort the list of files in the directory that start with a number.
+      - Delete the file that's the oldest/lowest (first position after sorting)
+    """
+    allfiles = sorted(Path("backgroundimages/").glob("[0-9]*.py"))
     if len(allfiles) >= 7:
         try:
             os.remove(allfiles[0])
@@ -56,9 +74,16 @@ def delete_previous():
 
 
 if __name__ == '__main__':
+    try:
+        os.mkdir("backgroundimages")
+    except FileExistsError:
+        pass
     check_date()
+    # Run the program at midnight
     schedule.every().day.at("00:00").do(check_date)
+    # Run this program every sunday
     schedule.every().sunday.do(delete_previous)
+    # Generate log file at startup
     logging.basicConfig(filename="generate_background_logs.log", level=logging.INFO)
     while True:
         schedule.run_pending()
